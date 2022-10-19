cff896d419bfcc2c1c123a000fb5f06e01b4bf82
KonstantinArabadzhi <kostyaarabadji@gmail.com>
2022-10-19 09:57:36 +0300

    change stats script

--- src/getAppStats.js
+++ src/getAppStats.js
@@ -1,7 +1,10 @@
 const dotenv = require("dotenv");
 dotenv.config();
 require("./connectDB");
+const mongoose = require("mongoose");
 
 const matchesService = require("./services/matchesService");
 
 matchesService.getAppStats();
+
+mongoose.connection.close();
