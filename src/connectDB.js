0222b8a7777e3ab4b7f1a432521c129def3c44a5
KonstantinArabadzhi <kostyaarabadji@gmail.com>
2023-10-12 21:45:15 +0300

    feat: update db name connection

--- src/connectDB.js
+++ src/connectDB.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 const Logger = require("./services/loggerService");
 
-const { DB_HOST, DB_PASSWORD, DB_USER } = process.env;
+const { DB_HOST, DB_PASSWORD, DB_USER, DB_NAME } = process.env;
 
 async function connect() {
   try {
     await mongoose.connect(
-      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority`,
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
     );
     Logger.log("mongodb connected successfully");
   } catch (error) {
